6c0e1450f9a6d27d80d18c7d89b80e1f1c540a3d
Ujwal Gowda V <ujwalgowda2002v@gmail.com>
2024-11-27 20:21:51 +0530

    Controller changes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,70 +1,54 @@
 const Admin = require("../models/admin");
 const jwt = require("jsonwebtoken");
 
-async function signup(req, res){
-    // const { userName, password } = req.body;
-    // try{
-    //     const adminExists = await Admin.findOne({ userName });
-    //     if(adminExists){
-    //         return res.status(400).json({ message: "Admin already exists" });
-    //     }
-
-    //     const hashedPassword = await bcrypt.hash(password, 10);
-    //     const user = new Admin({ userName, password: hashedPassword });
-    //     await user.save();
-    //     res.status(201).json({ message: "Admin created" });
-    // }
-    // catch(error){
-    //     res.status(500).json({ message: "Error in creating user" });
-    // }
-
-
+async function signup(req, res) {
     const { userName, password } = req.body;
-    const adminExists = await Admin.findOne({ userName });
-    
-    try{
-        if(adminExists){
-            res.status(400).json({ message: "Admin already exists" });
+
+    try {
+        const adminExists = await Admin.findOne({ userName });
+
+        if (adminExists) {
+            return res.status(400).json({ message: "Admin already exists" });
         }
-    } catch(error){
-        console.log(error.message);
-    }
 
-    const admin = await Admin.create({ userName, password });
+        const admin = await Admin.create({ userName, password });
 
-    if(admin){
-        console.log("Admin created");
-        const token = jwt.sign({ name: admin._id }, "youcantdecodethis", { expiresIn: "2h" });
-        res.cookie( 'name', admin.userName );
-        console.log(admin.userName);
-        res.status(201).json({ message: "Admin created" });
-        res.json({ token });
-    }
-    else{
-        res.status(400).json({ message: "Invalild admin data" });
+        if (admin) {
+            console.log("Admin created");
+            const token = jwt.sign({ name: admin._id }, "youcantdecodethis");
+            console.log(admin.userName);
+            return res.status(201).json({ message: "Admin created", token });
+        } else {
+            return res.status(400).json({ message: "Invalid admin data" });
+        }
+    } catch (error) {
+        console.error("Error in signup:", error.message);
+        return res.status(500).json({ message: "Server error, please try again later" });
     }
 }
 
-async function login(req, res){
+async function login(req, res) {
     const { userName, password } = req.body;
-    console.log("Request Body:", req.body);
 
+    try {
+        console.log("Request Body:", req.body);
 
-    const admin = await Admin.findOne({ userName });
-    console.log("Admin Found:", admin);
-    if (admin) {
-        console.log("Password Match:", await admin.matchPassword(password));
-    }
-    if(admin && (await admin.matchPassword(password))){
-        const token = jwt.sign({ name: admin._id }, "youcantdecodethis", { expiresIn: "2h" });
-        res.cookie( 'name', admin.userName );
-        console.log(admin.userName);
-        console.log("Admin logged in");
-        res.json({ token });
-    }
-    else{
-        res.status(401).json({ message: "Invalid credentials" });
+        const admin = await Admin.findOne({ userName });
+        console.log("Admin Found:", admin);
+
+        if (admin && (await admin.matchPassword(password))) {
+            const token = jwt.sign({ name: admin._id }, "youcantdecodethis");
+            console.log(admin.userName);
+            console.log("Admin logged in");
+            return res.json({ token });
+        } else {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+    } catch (error) {
+        console.error("Error in login:", error.message);
+        return res.status(500).json({ message: "Server error, please try again later" });
     }
 }
+
 
 module.exports = { signup, login };
