daee68992f39d96ffe4a3695bc8d6c371ce9905f
Ujwal Gowda V <ujwalgowda2002v@gmail.com>
2024-11-27 21:09:53 +0530

    CORS

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ connectMongoDB();
 
 const app = express();
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST'],
-  }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
